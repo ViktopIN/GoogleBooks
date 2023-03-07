6382782ee39cdc6bb3972db3ec4d55bf63f2caa9
Viktor <viktopiniaginya@gmail.com>
2023-03-07 12:50:37 +0300

    GoogleResponseModel create

--- GoogleBooks/Sources/Scenes/Search/Model/GoogleResponseModel.swift
+++ GoogleBooks/Sources/Scenes/Search/Model/GoogleResponseModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+// MARK: - GoogleResponseModel
+struct GoogleResponseModel: Codable {
+    let items: [Item] //
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let selfLink: String
+    let volumeInfo: VolumeInfo
+    let accessInfo: AccessInfo
+}
+
+// MARK: - AccessInfo
+struct AccessInfo: Codable {
+    let webReaderLink: String
+}
+
+// MARK: - VolumeInfo
+struct VolumeInfo: Codable {
+    let title: String
+    let authors: [String]
+    let imageLinks: ImageLinks
+}
+
+// MARK: - ImageLinks
+struct ImageLinks: Codable {
+    let smallThumbnail: String
+}
