7b0868fabc4e1706cd4db6b7d8fc2812f686b4d1
Viktor <viktopiniaginya@gmail.com>
2023-03-07 11:25:07 +0300

    Modify appDelegate, add SnapKit

--- GoogleBooks/Extensions/UILabel+Extension.swift
+++ GoogleBooks/Extensions/UILabel+Extension.swift
@@ -5,4 +5,40 @@
 //  Created by Виктор on 07.03.2023.
 //
 
-import Foundation
+import UIKit.UILabel
+
+extension UILabel {
+    enum MarkProFonts {
+        case heavy, bold, medium, regular
+    }
+    convenience init(constant text: String = "",
+                     with textSize: CGFloat,
+                     and markProFontType: MarkProFonts,
+                     _ fontColor: UIColor = .black,
+                     adjustFontSizeToFit: Bool = false) {
+        self.init()
+        translatesAutoresizingMaskIntoConstraints = false
+        self.text = text
+        self.textColor = fontColor
+        self.adjustsFontSizeToFitWidth = adjustFontSizeToFit
+        switch markProFontType {
+        case .heavy:
+            self.font = UIFont.markProHeavy(ofSize: textSize)
+        case .bold:
+            self.font = UIFont.markProBold(ofSize: textSize)
+        case .medium:
+            self.font = UIFont.markProMedium(ofSize: textSize)
+        case .regular:
+            self.font = UIFont.markProRegular(ofSize: textSize)
+        }
+    }
+    
+    open override func copy() -> Any {
+        let copy = UILabel()
+        copy.text = self.text
+        copy.textColor = self.textColor
+        copy.font = self.font
+        copy.translatesAutoresizingMaskIntoConstraints = false
+        return copy
+    }
+}
