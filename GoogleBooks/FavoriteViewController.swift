a5eb032d69663aa11be3520385b972d4374641bc
Viktor <viktopiniaginya@gmail.com>
2023-03-07 11:25:39 +0300

    Info plist configuration

--- GoogleBooks/FavoriteViewController.swift
+++ GoogleBooks/FavoriteViewController.swift
@@ -5,4 +5,13 @@
 //  Created by Виктор on 07.03.2023.
 //
 
-import Foundation
+import UIKit
+
+class FavoriteViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+    
+}
--- GoogleBooks/SearchViewController.swift
+++ GoogleBooks/SearchViewController.swift
@@ -5,4 +5,26 @@
 //  Created by Виктор on 07.03.2023.
 //
 
-import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    // MARK: - Infrastructure
+    
+    private lazy var searchView: SearchView = {
+        let searchView = SearchView(frame: CGRect(x: 0, y: 0, width: 320, height: 480))
+        return searchView
+    }()
+    
+    // MARK: - LifeCycle
+    
+    override func loadView() {
+        self.view = searchView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+    
+}
